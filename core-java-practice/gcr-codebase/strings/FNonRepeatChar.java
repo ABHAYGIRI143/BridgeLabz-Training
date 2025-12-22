f9c724f648b7d4e74e052ae8dd8480f1bd4d0712
ABHAYGIRI143 <abhaygiri656@gmail.com>
2025-12-22 20:51:47 +0530

    [abhay] add .ADDED Array Question Completed

---
+++ core-java-practice/gcr-codebase/strings/FNonRepeatChar.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class FNonRepeatChar {
+
+    public static char findFirstNonRepeatCh(String str) {
+        
+        int[] freqArr = new int[256];
+        
+        // Count frequency
+        for(int i=0; i <str.length(); i++) {
+            freqArr[str.charAt(i)]++;
+        }
+
+        // Find first non-repeating character
+        for(int i=0;i<str.length();i++){
+            if(freqArr[str.charAt(i)]==1){
+                return str.charAt(i);
+            }
+        }
+
+        return '0'; // No non-repeating character
+    }
+
+    //Main method
+    public static void main(String[] args) {
+
+        //Create Scanner Object
+        Scanner sc=new Scanner(System.in);
+
+        //take input from user
+        System.out.print("Enter a string: ");
+        String str=sc.nextLine();
+
+        //call methods
+        char res=findFirstNonRepeatCh(str);
+
+        if(res!='0') {
+            System.out.println("First non-repeating character is: " + res);
+        } else {
+            System.out.println("No non-repeating character found");
+        }
+
+        //Close Scanner Object
+        sc.close();
+    }
+}
