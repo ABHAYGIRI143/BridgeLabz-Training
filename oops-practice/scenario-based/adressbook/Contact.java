d377f5eec9f0d4da6cde6d5f7d2359aaebf36c04
ABHAYGIRI143 <abhaygiri656@gmail.com>
2026-01-20 00:53:38 +0530

    [abhay] add .Added Address Book Completed

--- oops-practice/scenario-based/adressbook/Contact.java
+++ oops-practice/scenario-based/adressbook/Contact.java
@@ -1,54 +1,50 @@
 
-class InvalidDetails extends Exception{
-	public InvalidDetails(String message) {
-		super(message);
-	}
-}
 
-public class Contact{
-	private String firstName;
-	private String lastName;
-	private String city;
-	private String state;
-	protected String phoneNumber;
-	protected String email;
-	
-	Contact(String firstName,String lastName,String city,String state,String phoneNumber,String email) throws InvalidDetails{
-		if(phoneNumber.length()!=10 || !email.contains("@gmail.com")) {
-			throw new  InvalidDetails ("Invalid Details");
-		}
-		
-		this.firstName = firstName;
-		this.lastName= lastName;
-		this.city=city;
-		this.state=state;
-		this.phoneNumber=phoneNumber;
-		this.email=email;
-	}
-	
-	String  getFirstName() {
-		return firstName;
-	}
-	
-	String  getLastName() {
-		return lastName;
-	}
-	
-	String  getPhoneNumber() {
-		return phoneNumber;
-	}
-	
-	String  getEmail() {
-		return email;
-	}
-	
-	void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber=phoneNumber;
-	}
-	
-	void setEmail(String email) {
-		this.email=email;
-	}
-	
-	
+public class Contact {
+
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String phoneNumber;
+    private String email;
+
+    public Contact(String firstName, String lastName, String address,
+                   String city, String state,
+                   String phoneNumber, String email) {
+    	
+    	
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    
+
+    public String getAddress() {
+        return city;
+    }
+
+    public void displayContact() {
+        System.out.println("Name: " + firstName + " " + lastName);
+        System.out.println("Address: " + address);
+        System.out.println("City: " + city);
+        System.out.println("State: " + state);
+        System.out.println("Phone: " + phoneNumber);
+        System.out.println("Email: " + email);
+        System.out.println("--------------------------");
+    }
 }
